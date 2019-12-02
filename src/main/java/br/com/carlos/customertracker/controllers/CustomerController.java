649ef08ac764d6e6a39c01c4a5e30d60ad7a13f8
Carlos Eduardo <caduleonel1988@gmail.com>
2019-12-02 18:16:20 -0300

    Modificando a action listCustomers para retornar uma lista de Customers

--- src/main/java/br/com/carlos/customertracker/controllers/CustomerController.java
+++ src/main/java/br/com/carlos/customertracker/controllers/CustomerController.java
@@ -1,16 +1,32 @@
 package br.com.carlos.customertracker.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.carlos.customertracker.dao.CustomerDAO;
+import br.com.carlos.customertracker.entities.Customer;
 
 @Controller
 @RequestMapping(value = "/customer")
 public class CustomerController {
 
-	@RequestMapping(value = "list")
+	// inject the customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@RequestMapping(value = "/list")
 	public String listCustomers(Model model) {
-			
+		
+		// get customers from the dao
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		// add the customers to the model
+		model.addAttribute(customers);
+		
 		return "list-customers";
 
 	}
