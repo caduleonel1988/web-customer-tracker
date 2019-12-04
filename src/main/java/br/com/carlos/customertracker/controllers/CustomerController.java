88cef1308aaaaa706a915d7d14114afe82871c1a
Carlos Eduardo <caduleonel1988@gmail.com>
2019-12-03 23:14:23 -0300

    Criando a pagina index.jsp (que por enquanto redireciona automaticamente para a url customer/list. Também adicionado o scopo de request na classe CustomerController.

--- src/main/java/br/com/carlos/customertracker/controllers/CustomerController.java
+++ src/main/java/br/com/carlos/customertracker/controllers/CustomerController.java
@@ -3,15 +3,18 @@ package br.com.carlos.customertracker.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.WebApplicationContext;
 
 import br.com.carlos.customertracker.dao.CustomerDAO;
 import br.com.carlos.customertracker.entities.Customer;
 
 @Controller
 @RequestMapping(value = "/customer")
+@Scope(value=WebApplicationContext.SCOPE_REQUEST)
 public class CustomerController {
 
 	// inject the customer dao
